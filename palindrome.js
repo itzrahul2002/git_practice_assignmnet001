f880a0d86a9a3ee8df24b1c727093891dace3bd7
unknown <rahulthakurking2017@gmail.com>
2022-12-09 03:45:12 +0530

    created problem prime and palindrome

---
+++ palindrome.js
@@ -0,0 +1,13 @@
+N=6;
+str="nrupul";
+
+let bag="";
+ for(i=str.length;i>0;i--){
+   bag+=str[i]
+ }
+if(bag==str){
+  console.log("it is palindrome")
+}
+else{
+  console.log("it is not a palindrome");
+}
---
+++ prime.js
@@ -0,0 +1,14 @@
+let num=18;
+let count=0;
+
+for(let i=0;i<=num;i++){
+if(num%i==0){
+count++;
+}
+  
+}if(count==2){
+console.log("It is a prime number");
+}
+else{
+console.log("It is not prime number");
+}
